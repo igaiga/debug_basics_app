da4ef1945e167cd38337764b9eaae0859b966aca
Kuniaki IGARASHI <igaiga@gmail.com>
2015-08-31 10:09:03 +0900

    Sending mail test

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
   # GET /books.json
   def index
     @books = Book.all
+    # ExampleMailer.hello.deliver_now # mail sending test
   end
 
   # GET /books/1
